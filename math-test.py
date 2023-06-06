93d690ef9f410ef2c6be9829a2f02a2a23aff7aa
Mike Wynn <34527764+mikewynn2@users.noreply.github.com>
2023-06-06 11:17:49 -0700

    Update math-test.py to refine
    refined old math game with new and improved abilities.

--- math-test.py
+++ math-test.py
@@ -1,36 +1,37 @@
 import random
+import time
 
-digit1 = random.randint(1,12)
+digit1 = random.randint(1, 12)
 
-digit2 = int(input("What times tables are we working on today?(TYPE 0 FOR RANDOM NUMBER)"))
+digit2 = int(input("What times tables are we working on today?(TYPE 0 FOR RANDOM NUMBER) "))
 if digit2 == 0:
-    digit2 = random.randint(1,12)
-        
+    digit2 = random.randint(1, 12)
+
 pts = 0
-
 turns = 0
+start_time = time.time()
+while pts < 10:
 
-while pts < 30:
-    
-    print(digit1)
-    print("X")
-    print(digit2)
-    ans = int(input("=?"))
+    print("%d X %d =? " % (digit1, digit2))
+    ans = int(input("? "))
 
     if ans == digit1 * digit2:
         print("correct")
         pts = pts + 1
-        digit1 = random.randint(1,12)
+        digit1 = random.randint(1, 12)
         turns = turns + 1
 
     elif ans != digit1 * digit2:
         print("WRONG")
-        
-        digit1 = random.randint(1,12)
+        correction = digit1 * digit2
+        print(" The correct answer is " + str(correction))
+
+        digit1 = random.randint(1, 12)
         turns = turns + 1
 
 
-if pts == 30:
+if pts == 10:
+    timer = time.time() - start_time
     print("CONGRATS!!! You Passed!!!")
-    print("It took you " + str(turns) + " turns to get to 5 points!")
-
+    print("It took you " + str(turns) + " turns to get to %d points!" % (pts))
+    print("It took %d seconds to get to %s points!" % (timer, pts))
